1b8864b9a1a976bb0c56b43f780c6723eaa9e894
purav-parekh <purav98@gmail.com>
2024-01-30 15:50:31 -0500

    add MUI & styles

--- src/components/LandingSection.tsx
+++ src/components/LandingSection.tsx
@@ -1,32 +1,44 @@
-import { Grid, Typography } from "@mui/material";
+import { Link, List, ListItem, ListItemText, ListSubheader, Stack, Typography } from "@mui/material";
 
 export default function LandingSection() {
 	return (
-		<Grid container style={{ border: "1px solid black" }}>
-			<Grid xs={4} style={{ border: "1px solid red" }}>
-				<Typography variant="h2" gutterBottom>
+		<Stack direction="row">
+			<Stack className="text-center p-2">
+				<Typography className="italic" variant="h2" gutterBottom>
 					SITExpress
 				</Typography>
-				<Typography variant="h3" gutterBottom>
+				<Typography variant="h4" gutterBottom>
 					For Project Investment Analysis
 				</Typography>
-				<Typography variant="h3" gutterBottom>
-					Powered by E3 https://e3.nist.gov/
+
+				<Typography variant="h5" gutterBottom>
+					Powered by E3
+					<Link href="https://e3.nist.gov/" underline="none" color="inherit" target="_blank" rel="noopener">
+						https://e3.nist.gov/
+					</Link>
 				</Typography>
-			</Grid>
-			<Grid xs={8} style={{ border: "1px solid yellow" }}>
-				<Typography variant="h4" className="rectangleText">
+			</Stack>
+			<Stack className="p-2 bg-sky-500 flex justify-center">
+				<Typography variant="h5" className="text-center">
 					Smart Investment Tool Express (SITExpress) completes investment analysis based on NIST Advanced Manufacturing
 					Series 200-5 to identify economical projects/investments.
 				</Typography>
 				<br />
-				<Typography variant="h4" className="rectangleText2">
-					Calculated metrics include: <br />
-					Net Present Value (NPV) <br />
-					Internal Rate of Return (IRR) <br />
-					Payback Period (PBP)
-				</Typography>
-			</Grid>
-		</Grid>
+
+				<Typography variant="h5">Calculated metrics include:</Typography>
+				<List dense>
+					<ListSubheader></ListSubheader>
+					<ListItem>
+						<ListItemText primary="Net Present Value (NPV)" />
+					</ListItem>
+					<ListItem>
+						<ListItemText primary="Internal Rate of Return (IRR)" />
+					</ListItem>
+					<ListItem>
+						<ListItemText primary="Payback Period (PBP)" />
+					</ListItem>
+				</List>
+			</Stack>
+		</Stack>
 	);
 }
