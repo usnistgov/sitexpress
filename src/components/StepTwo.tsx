5f224d1b90ab1056b41f9176a591e4c3e2f4077d
purav-parekh <purav98@gmail.com>
2024-03-19 10:47:13 -0400

    add confirmation modal on reset

--- src/components/StepTwo.tsx
+++ src/components/StepTwo.tsx
@@ -1,12 +1,30 @@
-import { Button, Stack, Typography } from "@mui/material";
+import RefreshIcon from "@mui/icons-material/Refresh";
+import { Box, Button, Modal, Stack, Typography } from "@mui/material";
 import { useRef, useState } from "react";
 import { E3Request, toE3Object } from "../data/E3Request";
 import DataGrid from "./DataGrid";
 import BasicTooltip from "./Tooltip";
+
+const style = {
+	position: "absolute" as "absolute",
+	top: "50%",
+	left: "50%",
+	transform: "translate(-50%, -50%)",
+	width: 400,
+	bgcolor: "background.paper",
+	border: "2px solid #000",
+	boxShadow: 24,
+	p: 4,
+};
+
 // @ts-ignore
 export default function StepTwo(props) {
 	const { project, getResults } = props;
 	const [gridData, setGridData] = useState([]);
+	const [open, setOpen] = useState(false);
+
+	const handleOpen = () => setOpen(true);
+	const handleClose = () => setOpen(false);
 
 	// @ts-ignore
 	const transformTableData = (data, alts = 3) => {
@@ -55,6 +73,7 @@ export default function StepTwo(props) {
 		// @ts-ignore
 		gridRef?.current.handleReset();
 		setGridData([]);
+		handleClose();
 	};
 	return (
 		<div>
@@ -70,10 +89,35 @@ export default function StepTwo(props) {
 				<Stack className="flex justify-center text-center p-10 ">
 					<Stack direction="column" className="ml-auto">
 						<span>
-							<Button variant="contained" className="" onClick={handleReset}>
-								Reset Table
+							<Button variant="contained" className="reset-btn" onClick={handleOpen}>
+								<RefreshIcon className="" fontSize="small" />
 							</Button>
-							<BasicTooltip title="text" />
+
+							<Modal
+								open={open}
+								onClose={handleClose}
+								aria-labelledby="modal-modal-title"
+								aria-describedby="modal-modal-description"
+							>
+								<Box sx={style}>
+									<Typography id="modal-modal-title" variant="h6" component="h2">
+										Are you sure you want to reset the table?
+									</Typography>
+									<Typography id="modal-modal-description" sx={{ mt: 2 }}>
+										You will lose all your entered data.
+									</Typography>
+									<br />
+									<span className="flex justify-around">
+										<Button variant="contained" className="" onClick={handleReset} color="error">
+											Yes
+										</Button>
+										<Button variant="contained" className="" onClick={handleClose}>
+											No
+										</Button>
+									</span>
+								</Box>
+							</Modal>
+							<BasicTooltip title="Resets the table" />
 						</span>
 						<br />
 					</Stack>
