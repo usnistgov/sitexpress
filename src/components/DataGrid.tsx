c66b89e743eb3c5a59418723fd1e0fc953bd8cd8
purav-parekh <purav98@gmail.com>
2024-04-30 11:57:48 -0400

    refactor code for the input table

--- src/components/DataGrid.tsx
+++ src/components/DataGrid.tsx
@@ -14,30 +14,42 @@ const generateData = (
 	let headerOnly = existingData[0];
 
 	if (data.length === 0) {
-		let header = new Map();
-		header.set("year", "");
-		header.set("base-cost", "Cost ($)");
-		header.set("base-rev", "Revenue ($)");
+		// let header = new Map();
+		// header.set("year", "");
+		// header.set("base-cost", "Cost ($)");
+		// header.set("base-rev", "Revenue ($)");
+
+		const header: InputTableData = {
+			year: "",
+			"base-cost": "Cost ($)",
+			"base-rev": "Revenue ($)",
+		};
 
 		for (let i = 1; i <= alts; i++) {
-			header.set(`alt${i}-cost`, "Cost ($)");
-			header.set(`alt${i}-rev`, "Revenue ($)");
+			header[`alt${i}-cost` as keyof InputTableData] = "Cost ($)";
+			header[`alt${i}-rev` as keyof InputTableData] = "Revenue ($)";
 		}
 
-		data.push(Object.fromEntries(header));
+		data.push(header);
 
 		for (let y = 0; y <= years; y++) {
-			let yearData = new Map();
-			y === 0 ? yearData.set("year", `Initial Investment`) : yearData.set("year", y.toString());
-			yearData.set("base-cost", "");
-			y === 0 ? yearData.set("base-rev", 0) : yearData.set("base-rev", "");
+			// let yearData = new Map();
+			// y === 0 ? yearData.set("year", `Initial Investment`) : yearData.set("year", y.toString());
+			// yearData.set("base-cost", "");
+			// y === 0 ? yearData.set("base-rev", 0) : yearData.set("base-rev", "");
+
+			const yearData: Record<string, string | number> = {
+				year: y === 0 ? "Initial Investment" : y.toString(),
+				"base-cost": "",
+				"base-rev": y === 0 ? 0 : "",
+			};
 
 			for (let i = 1; i <= alts; i++) {
-				yearData.set(`alt${i}-cost`, "");
-				y === 0 ? yearData.set(`alt${i}-rev`, 0) : yearData.set(`alt${i}-rev`, "");
+				yearData[`alt${i}-cost`] = "";
+				yearData[`alt${i}-rev`] = y === 0 ? 0 : "";
 			}
 
-			data.push(Object.fromEntries(yearData));
+			data.push(yearData);
 		}
 	} else {
 		let header: InputTableData = headerOnly;
@@ -47,15 +59,20 @@ const generateData = (
 				header[`alt${i}-rev` as keyof InputTableData] = "Revenue ($)";
 			}
 			let yearData: InputTableData[] = [];
+			// yearsOnly.forEach((year) => {
+			// 	const x = { ...year };
+			// 	for (let i = oldAlts + 1; i <= alts; i++) {
+			// 		x[`alt${i}-cost` as keyof InputTableData] = "";
+			// 		x.year.startsWith("Initial")
+			// 			? (x[`alt${i}-rev` as keyof InputTableData] = "0")
+			// 			: (x[`alt${i}-rev` as keyof InputTableData] = "");
+			// 	}
 			yearsOnly.forEach((year) => {
-				const x = { ...year };
 				for (let i = oldAlts + 1; i <= alts; i++) {
-					x[`alt${i}-cost` as keyof InputTableData] = "";
-					x.year.startsWith("Initial")
-						? (x[`alt${i}-rev` as keyof InputTableData] = "0")
-						: (x[`alt${i}-rev` as keyof InputTableData] = "");
+					year[`alt${i}-cost` as keyof InputTableData] = "";
+					year[`alt${i}-rev` as keyof InputTableData] = year.year.startsWith("Initial") ? "0" : "";
 				}
-				yearData.push(x);
+				yearData.push(year);
 			});
 			headerOnly = header;
 			yearsOnly = yearData;
@@ -71,16 +88,28 @@ const generateData = (
 		// add or remove a row
 		if (years > oldYears) {
 			for (let i = oldYears + 1; i <= years; i++) {
-				let yearData = new Map();
-				yearData.set("year", i.toString());
-				yearData.set("base-cost", "");
-				yearData.set("base-rev", "");
+				// let yearData = new Map();
+				// yearData.set("year", i.toString());
+				// yearData.set("base-cost", "");
+				// yearData.set("base-rev", "");
 
-				for (let i = 1; i <= alts; i++) {
-					yearData.set(`alt${i}-cost`, "");
-					yearData.set(`alt${i}-rev`, "");
+				const yearData: Record<string, string | number> = {
+					year: i.toString(),
+					"base-cost": "",
+					"base-rev": "",
+				};
+
+				for (let j = 1; j <= alts; j++) {
+					yearData[`alt${j}-cost`] = "";
+					yearData[`alt${j}-rev`] = "";
 				}
-				yearsOnly.push(Object.fromEntries(yearData));
+				yearsOnly.push(yearData as InputTableData);
+
+				// for (let i = 1; i <= alts; i++) {
+				// 	yearData.set(`alt${i}-cost`, "");
+				// 	yearData.set(`alt${i}-rev`, "");
+				// }
+				// yearsOnly.push(Object.fromEntries(yearData));
 			}
 		} else if (years < oldYears) {
 			for (let i = 0; i < oldYears - years; i++) {
@@ -121,37 +150,71 @@ const headerRow = (alts: number, names: altNames) => {
 		{ type: "header", text: splitString(names?.["alt0"])[0] || "Base", colSpan: 2 },
 		{ type: "header", text: splitString(names?.["alt0"])[1] || "Case" },
 	];
+	// for (let i = 1; i <= alts; i++) {
+	// 	header.push(
+	// 		{ type: "header", text: splitString(names?.[`alt${i}` as keyof altNames])[0] || "Alternati", colSpan: 2 },
+	// 		{ type: "header", text: splitString(names?.[`alt${i}` as keyof altNames])[1] || `ve ${i}` },
+	// 	);
+	// }
 	for (let i = 1; i <= alts; i++) {
+		const altName = names?.[`alt${i}` as keyof altNames];
 		header.push(
-			{ type: "header", text: splitString(names?.[`alt${i}` as keyof altNames])[0] || "Alternati", colSpan: 2 },
-			{ type: "header", text: splitString(names?.[`alt${i}` as keyof altNames])[1] || `ve ${i}` },
+			{ type: "header", text: splitString(altName)[0] || `Alternati`, colSpan: 2 },
+			{ type: "header", text: splitString(altName)[1] || `ve ${i}` },
 		);
 	}
+
 	return {
 		rowId: "header",
 		cells: header,
 	};
 };
 
-const getRows = (data: InputTableData[], alts: number, names: altNames) => [
-	headerRow(alts, names),
-	...data.map((dataPoint, idx: number) => {
-		const cells = [];
-		for (const [key, value] of Object.entries(dataPoint)) {
-			let obj = {};
-			if (value === "Initial Investment" || value === "Cost ($)" || value === "Revenue ($)" || key === "year") {
-				obj = { type: "text", text: value, nonEditable: true };
-			} else {
-				obj = { type: "number", text: value, value: value };
+const getRows = (data: InputTableData[], alts: number, names: altNames) => {
+	return [
+		headerRow(alts, names),
+		...data.map((dataPoint, idx: number) => {
+			const cells = [];
+			for (const [key, value] of Object.entries(dataPoint)) {
+				let obj = {};
+				if (value === "Initial Investment" || value === "Cost ($)" || value === "Revenue ($)" || key === "year") {
+					obj = { type: "text", text: value, nonEditable: true };
+				} else {
+					obj = { type: "number", text: value, value: value };
+				}
+				if ((/alt[1-5]-rev/.test(key) || key === "base-rev") && dataPoint.year === "Initial Investment") {
+					obj = { type: "number", text: value, value: +value, nonEditable: true };
+				}
+				cells.push(obj);
 			}
-			if ((/alt[1-5]-rev/.test(key) || key === "base-rev") && dataPoint.year === "Initial Investment") {
-				obj = { type: "number", text: value, value: +value, nonEditable: true };
-			}
-			cells.push(obj);
-		}
-		return { rowId: idx, cells };
-	}),
-];
+			return { rowId: idx, cells };
+		}),
+	];
+};
+
+// const getRows = (data: InputTableData[], alts: number, names: altNames) => {
+// 	return [
+// 		headerRow(alts, names),
+// 		...data.map((dataPoint, idx) => ({
+// 			rowId: idx,
+// 			cells: Object.entries(dataPoint).map(([key, value]) => {
+// 				console.log(dataPoint, [key, value]);
+// 				const isInitialInvestment = dataPoint.year === "Initial Investment";
+// 				const isRevenue = /alt[1-5]-rev/.test(key) || key === "base-rev";
+// 				const isNonEditable = isInitialInvestment && isRevenue;
+
+// 				return {
+// 					type: typeof value === "number" ? "number" : "text",
+// 					text: value,
+// 					value: isNonEditable ? +value : value,
+// 					nonEditable:
+// 						isInitialInvestment || value === "Cost ($)" || value === "Revenue ($)" || key === "year" || isNonEditable,
+// 				};
+// 			}),
+// 		})),
+// 	];
+// };
+
 const applyChangesToData = (changes: CellChange<NumberCell>[], prevData: InputTableData[]) => {
 	changes.forEach((change) => {
 		const dataIndex = change?.rowId;
